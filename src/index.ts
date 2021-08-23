2feb03105c30c97ef52d94e6c2e2b9c66da9a9a7
Pricop Laurențiu <laurcons@outlook.com>
2021-08-23 11:29:52 +0300

    Yeah whoops this too

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { setValidationLogger } from "./middlewares/validation-middleware";
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json());
 app.use(morgan("dev"));
